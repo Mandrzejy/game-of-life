66adafd6ed574065bbcad0fca2f325c222cbdaa8
Andrzej Pregowski <kablujena1234567@gmail.com>
2021-12-11 12:12:12 +0100

    nope

--- main.c
+++ main.c
@@ -1,70 +1,74 @@
 #include<stdio.h>
-#include<stdbool.h>
-#define WIDTH 20
-#define HEIGHT 20
+#include<stdlib.h>
+#define WIDTH 30
+#define HEIGHT 40
 
 #define PRINT_BLOCK printf("\033[0;37m\u2588\u2588")
 #define PRINT_SPACE printf("\033[0;30m\u2588\u2588");
+#define CLS printf("\033[H\033[J")
 
 
-
-void copy(bool what[20][20],bool where[20][20]){}
-bool grid[WIDTH][HEIGHT];
-bool _next_gen[WIDTH][HEIGHT];
-bool ptr[WIDTH][HEIGHT];
-void print_grid(){	
-	for(int y=0;y<HEIGHT;y++){
-		for(int x=0;x<WIDTH;x++){
+void copy(int what[WIDTH+2][HEIGHT+2],int where[WIDTH+2][HEIGHT+2]){
+	for(int y=0;y<HEIGHT+2;y++){
+		for(int x=0;x<WIDTH+2;x++){
+			where[y][x]=what[y][x];
+	
+		}
+	}
+	
+}
+int grid[WIDTH+2][HEIGHT+2];
+int _next_gen[WIDTH+2][HEIGHT+2];
+void print_grid(){
+	CLS;	
+	for(int y=1;y<HEIGHT+1;y++){
+		for(int x=1;x<WIDTH+1;x++){
 			grid[y][x]==1?PRINT_BLOCK:PRINT_SPACE;
 		}
 		printf("\n");
 	}
 }
 
+
 void next_gen(){
-	for(int y=0;y<HEIGHT;y++){
-		for(int x=0;x<WIDTH;x++){
-			int neighbors=0;
-			if(y==0){
-				if(x==0){
-					neighbors=grid[1][1]+grid[0][1]+grid[1][0];
-				}
-				if(x==WIDTH-1){
-					neighbors=grid[0][WIDTH-2]+grid[1][WIDTH-1]+grid[1][WIDTH-2];
+	for(int y=1;y<HEIGHT+1;y++){
+		for(int x=1;x<WIDTH+1;x++){
+			//where
+			int n=grid[y][x-1]+grid[y][x+1]+ \
+			      grid[y-1][x]+grid[y-1][x-1]+grid[y-1][x+1]+\
+			      grid[y+1][x]+grid[y+1][x-1]+grid[y+1][x+1];
+
+
+				if(n==3 || n==2){
+					if(grid[y][x]==0){if(n==3){_next_gen[y][x]=1;}else{_next_gen[y][x]=0;}}
+					else{_next_gen[y][x]=1;}
+
 				}
 				else{
-
-					neighbors=grid[y][x-1]+grid[y+1][x]+grid[y+1][x-1]+grid[y+1][x+1]+grid[y][x+1];
-				}				
-			}
-			else if(x==0){
-					
-			}
-			else if(y==HEIGHT-1){
-
-			}
-			else if(x==WIDTH -1){
-
-			}
-			else{
-
-			}
-			//neighbors 	
+					_next_gen[y][x]=0;
+				}
 		}
 
 
-	}	
+	}
 	copy(_next_gen,grid);
 }
 
-int main(){
-	grid[2][4]=1;
-	grid[2][3]=1;
-	grid[3][4]=1;
-	grid[3][7]=1;
-	next_gen();
-	print_grid();
+int main(){	
+	
+	grid[10][3]=1;
+	grid[10][4]=1;
+	grid[10][5]=1;
+	grid[12][4]=1;
+	
 
+	
+	char c;
+	while(1){
+		print_grid();
+		next_gen();
+		while((c=getchar())!='\n'){if(c=='q'){exit(0);}};	
+	}
 
 
 
