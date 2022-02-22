b17b8f802d67ec1f1444df3c7ce0c9e52f7fb9d7
Andrzej Pręgowski <andrzej@arch.localdomain>
2022-02-22 21:41:49 +0100

    Rewritten most of the engine

--- main.c
+++ main.c
@@ -1,93 +1,76 @@
 #include<stdio.h>
 #include<stdlib.h>
-#define WIDTH 75
-#define HEIGHT 40
+#include<unistd.h>
+#include<ctype.h>
 
 #define PRINT_BLOCK printf("\033[0;37m\u2588\u2588")
-#define PRINT_SPACE printf("\033[0;30m\u2588\u2588");
+#define PRINT_SPACE printf("\033[0;30m\u2588\u2588")
 #define CLS printf("\033[H\033[J")
 #define NEWLINE printf("\n")
-#define ROW_SIZE WIDTH+2
-
 #define index(x,y,row_size) (y)*(row_size)+x
-int grid[(HEIGHT+2)*(WIDTH+2)];
-
-int next[(HEIGHT+2)*(WIDTH+2)];
 
 
-void copy(){
-	for(int y=1;y<HEIGHT+1;y++){
-		for(int x=1;x<WIDTH+1;x++){
-			grid[index(x,y,WIDTH+2)]=next[index(x,y,WIDTH+2)];
-		}
-	}
-}
 
-
-void print_grid(){
-	CLS;
-	for(int y=1;y<HEIGHT+1;y++){
-		for(int x=1;x<WIDTH+1;x++){
-			grid[index(x,y,WIDTH+2)]?PRINT_BLOCK:PRINT_SPACE;	
-		}
-		NEWLINE;
-	}
-}
-void compute_next(){
-	for(int y=1;y<HEIGHT+1;y++){
-		for(int x=1;x<WIDTH+1;x++){
-			int n=0;
-			n= grid[index(x-1,y,ROW_SIZE)]+grid[index(x+1,y,ROW_SIZE)] \
-			     + grid[index(x-1,y-1,ROW_SIZE)]+grid[index(x,y-1,ROW_SIZE)]+grid[index(x+1,y-1,ROW_SIZE)] \
-			     + grid[index(x-1,y+1,ROW_SIZE)]+grid[index(x,y+1,ROW_SIZE)]+grid[index(x+1,y+1,ROW_SIZE)];
-			
-			if(n==3 || n==2){
-				if(grid[index(x,y,ROW_SIZE)]==0){if(n==3){next[index(x,y,ROW_SIZE)]=1;}else{next[index(x,y,ROW_SIZE)]=0;}}
-				else{next[index(x,y,ROW_SIZE)]=1;}
-			}
-			else{
-				next[index(x,y,ROW_SIZE)]=0;
-			}		
-
-		}
-
-
-	}
-	copy();
-
-}
-
-int main(){	
-	#if 0	
-	grid[index(3,10,WIDTH+2)]=1;
-	grid[index(4,10,WIDTH+2)]=1;
-	grid[index(5,10,WIDTH+2)]=1;
-	grid[index(4,12,WIDTH+2)]=1;
-	#endif
-	#if 1
-	grid[index(5,5,ROW_SIZE)]=1;
-	grid[index(6,6,ROW_SIZE)]=1;
-	grid[index(7,6,ROW_SIZE)]=1;
-	grid[index(7,5,ROW_SIZE)]=1;
-	grid[index(7,4,ROW_SIZE)]=1;
-	#endif
-	#if 0
-	srand(112137829);
-	for(int y=1;y<HEIGHT+1;y++){
-		for(int x=1;x<WIDTH+1;x++){
-			grid[index(x,y,ROW_SIZE)]=rand()%2;
-		}
-	}
-
-
-	#endif
-	
+int main(int argc, char** argv){
+	if(argc==1){fprintf(stderr,"Filename not provided\n");return -1;}
+	FILE* f=fopen(argv[1],"r");
+	if(f==NULL){fprintf(stderr,"File couldn't be open\n");return -2;}
+	int ROW_SIZE;
+	int GEN;
+	fscanf(f,"%d",&ROW_SIZE);	
+	fscanf(f,"%d",&GEN);
+	int* grid = calloc((ROW_SIZE)*(ROW_SIZE),sizeof(int));	
+	int* next = calloc((ROW_SIZE)*(ROW_SIZE),sizeof(int));
+	if(grid==NULL || next==NULL){fprintf(stderr,"Memory error\n");return -3; }
 	char c;
-	while(1){
-		print_grid();
-		compute_next();
+	for(int y=0;y<ROW_SIZE;y++){
+		for(int x=0;x<ROW_SIZE;x++){
+			c=getc(f);
+			while(c=='\n' || c==' '){c=getc(f);}
+			grid[index(x,y,ROW_SIZE)]=(c=='0'?0:1);
+			
+		}
+	}
+	fclose(f);	
+	while((GEN>0)){
+
+		for(int y=0;y<ROW_SIZE;y++){
+			for(int x=0;x<ROW_SIZE;x++){
+				int s=0;
+				for(int dy=-1;dy<2;dy++){
+					for(int dx=-1;dx<2;dx++){
+						if(dx==dy && dx==0){continue;}
+						int new_x=(ROW_SIZE+x+dx)%ROW_SIZE;	
+						int new_y=(ROW_SIZE+y+dy)%ROW_SIZE;
+						s+=grid[index(new_x,new_y,ROW_SIZE)];
+					}
+
+				}		
+				if(s==3){next[index(x,y,ROW_SIZE)]=1;}
+				else if(s==2 && grid[index(x,y,ROW_SIZE)]){next[index(x,y,ROW_SIZE)]=1;}
+			}
 		
-		while((c=getchar())!='\n'){if(c=='q'){exit(0);}};	
+		}
+
+		
+		for(int y=0;y<ROW_SIZE;y++){
+			for(int x=0;x<ROW_SIZE;x++){
+				grid[index(x,y,ROW_SIZE)]=next[index(x,y,ROW_SIZE)];
+				next[index(x,y,ROW_SIZE)]=0;
+			}
+		}
+
+		
+		for(int y=0;y<ROW_SIZE;y++){
+			for(int x=0;x<ROW_SIZE;x++){
+				grid[index(x,y,ROW_SIZE)]?PRINT_BLOCK:PRINT_SPACE;
+			}
+			NEWLINE;
+		}
+		
+		usleep(50000);
+		GEN--;
+		if(GEN!=0){CLS;}
 	}
 
 
